773e4538149e814c249e4ab979c45233aa41399c
BalaBhargav329 <balabhargav960@gmail.com>
2026-09-08 15:47:07 +0530

    first program

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/first_program.py
@@ -0,0 +1,14 @@
+print("This is my first Program")
+print("My name is Bala Bhargav")
+print("My age is 22")
+print("My city is Nellore")
+print("My favorite programming language is Python")
+print("This is my first week of python Journey")
+print("My first week of Python was fun but challenging. I learned that Python has strict rules, unlike simple pseudocode. It takes time to remember exactly how to type things out, but seeing my program work perfectly feels amazing!")
+
+
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
