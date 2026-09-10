502fec020b515348f2e68f2f51d31727f83b2a1a
BalaBhargav329 <balabhargav960@gmail.com>
2026-09-10 16:00:16 +0530

    Day8

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/variables.py
@@ -0,0 +1,24 @@
+name = "Bhargav"
+name = "Balu"
+print(name)
+
+age = 22
+# My name is Balu and age is 22!
+print("My name is " + name + " and age is " , age , "!")
+
+# formatting String - f
+print(f"My name is {name} and age is {age}!")
+
+marks = 50
+marks = marks + 10
+print(marks)
+
+a, b, c = 10, 20, 30
+print(a, b, c)
+
+x = y = z = 100
+print(x, y, z)
+
+print(x + a) # 110
+print(x * y) # 10000
+
